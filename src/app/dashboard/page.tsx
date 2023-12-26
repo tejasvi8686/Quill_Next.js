d813f83e55fc810a2db65648edacbab3090026a1
tejasvi raj <tejasviraj8686@gmail.com>
2023-12-26 15:53:05 +0530

    added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,22 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import {db} from '@/db';
 import { redirect } from 'next/navigation'
-const Page = () => {
+import Dashboard from "@/components/Dashboard";
+const Page = async () => {
   const {getUser} = getKindeServerSession();
   const user = getUser()
   
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
+const dbUser = await db.user.findFirst({
+  where: {
+    id: user. id
+  }
+})
 
-  return <div>{user.email}</div>;
+if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
+  return <Dashboard /> ;
 };
 
 export default Page;
